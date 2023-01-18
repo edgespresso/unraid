148094d72d7eea3a47b79a139c11bcf2d15fa2f8
Edgespresso <github@lavorgna.net>
2023-01-18 18:19:54 -0500

    Added appdata

--- remoteBackup.sh
+++ remoteBackup.sh
@@ -21,6 +21,9 @@ echo "USB Path     : $usb_path"
 libvirt_file="/mnt/user/system/libvirt"
 echo "LIBVIRT File : $libvirt_file"
 
+appdata_path="/mnt/user/appdata"
+echo "APPDATA Path : $appdata_path"
+
 remote_path="/mnt/remotes/EDGENAS_unraid_backups"
 echo "Remote Path  : $remote_path"
 
@@ -28,14 +31,14 @@ echo "Remote Path  : $remote_path"
 cd $backup_path
 
 echo ""
-# Zip the USB files and LIBVIRT image
-echo "Zipping UNRAID USB files and the LIBVIRT image..."
-zip -rq $backup_path/unraid_backup-$(date +%d-%b-%Y).zip $usb_path $libvirt_file
+# Zip the USB files, LIBVIRT image and APPDATA folder
+echo "Zipping UNRAID USB files, LIBVIRT image and APPDATA folder..."
+zip -rq $backup_path/unraid_backup-$(date +%d-%b-%Y).zip $usb_path $libvirt_file $appdata_path
 
 echo "Copying today's backup to the remote backup server..."
 cp $backup_file $remote_path
 
-echo "Removing backup files that are ovewr 7 days old... "
+echo "Removing backup files that are over 7 days old... "
 find $backup_path -type f -name "*.zip" -mtime +7 -delete
 
 echo""
